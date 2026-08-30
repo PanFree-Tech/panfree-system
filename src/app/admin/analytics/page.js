f3d7978201c2fb4e0023bad33c6c0951e954694e
Luciana Noelia Da Silva <luzzdevictoria@gmail.com>
2026-08-30 19:44:53 -0300

    feat: agregar página de Analytics en /admin/analytics

---
+++ src/app/admin/analytics/page.js
@@ -0,0 +1,8 @@
+// src/app/admin/analytics/page.js
+'use client'
+
+import AnalyticsView from '../marketing/components/AnalyticsView'
+
+export default function AdminAnalyticsPage() {
+  return <AnalyticsView />
+}
