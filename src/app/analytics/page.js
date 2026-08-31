0103887339c7ecc450eee8c7bdc30c21a0c83ea3
Luciana Noelia Da Silva <luzzdevictoria@gmail.com>
2026-08-31 02:10:25 -0300

    fix: mover analytics a /admin/analytics (ruta correcta)

--- src/app/analytics/page.js
+++
@@ -1,8 +0,0 @@
-// src/app/admin/analytics/page.js
-'use client'
-
-import AnalyticsView from '../marketing/components/AnalyticsView'
-
-export default function AdminAnalyticsPage() {
-  return <AnalyticsView />
-}
